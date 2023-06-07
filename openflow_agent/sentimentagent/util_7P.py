c5e1530c90be9790116886041d28ea5597729e94
Zhuo Song <zhuosong@gmail.com>
2023-06-07 17:13:03 +0800

    0607

--- openflow_agent/sentimentagent/util_7P.py
+++ openflow_agent/sentimentagent/util_7P.py
@@ -43,12 +43,12 @@ You should output the results in JSON format, with the 7Ps as the main key and t
         template=template,
     )
     chain = LLMChain(llm=llm, prompt=prompt)
-    for i in txt_lines:
-        i_li = i.strip()
-        [b_re, b_tokens, b_cost, b_log] = call_openai(chain, i_li)
-        _log += b_log
-        _7P_str += re.sub(r"\n+", r"\n", b_re) + "\n"
-        _total_cost += b_cost
+    _txt_lines = "\n".join(txt_lines)
+    _li = _txt_lines.strip()
+    [b_re, b_tokens, b_cost, b_log] = call_openai(chain, _li)
+    _log += b_log
+    _7P_str += re.sub(r"\n+", r"\n", b_re) + "\n"
+    _total_cost += b_cost
     return [_log, _7P_str, _total_cost]
 
 
--- openflow_agent/sentimentagent/util_competitor.py
+++ openflow_agent/sentimentagent/util_competitor.py
@@ -43,12 +43,12 @@ You should output the results in JSON format, with the 'competitors' the main ke
         template=template,
     )
     chain = LLMChain(llm=llm, prompt=prompt)
-    for i in txt_lines:
-        i_li = i.strip()
-        [b_re, b_tokens, b_cost, b_log] = call_openai(chain, i_li)
-        _log += b_log
-        _competitor_str += re.sub(r"\n+", r"\n", b_re) + "\n"
-        _total_cost += b_cost
+    _txt_lines = "\n".join(txt_lines)
+    _li = _txt_lines.strip()
+    [b_re, b_tokens, b_cost, b_log] = call_openai(chain, _li)
+    _log += b_log
+    _competitor_str += re.sub(r"\n+", r"\n", b_re) + "\n"
+    _total_cost += b_cost
     return [_log, _competitor_str, _total_cost]
 
 
