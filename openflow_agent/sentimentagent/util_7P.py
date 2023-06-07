931cd48dcf5328c41bb3d481b0912bf012144922
Zhuo Song <zhuosong@gmail.com>
2023-06-07 12:09:28 +0800

    0607

--- openflow_agent/sentimentagent/util_7P.py
+++ openflow_agent/sentimentagent/util_7P.py
@@ -47,7 +47,7 @@ You should output the results in JSON format, with the 7Ps as the main key and t
         i_li = i.strip()
         [b_re, b_tokens, b_cost, b_log] = call_openai(chain, i_li)
         _log += b_log
-        total_cost += b_cost
+        _total_cost += b_cost
     return [_log, _7P_str, _total_cost]
 
 
