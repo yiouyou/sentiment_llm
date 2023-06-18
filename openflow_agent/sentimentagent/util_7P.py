506e82d548c4dd2b42efc17e6913a52db806325e
yiouyou <zhuosong@gmail.com>
2023-06-18 23:27:15 +0800

    0618

--- openflow_agent/sentimentagent/util_7P.py
+++ openflow_agent/sentimentagent/util_7P.py
@@ -5,14 +5,14 @@ key = "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"
 N_batch = 5
 
 
-def call_openai(chain, _content, _empty):
+def call_openai(chain, _content):
     from langchain.callbacks import get_openai_callback
     _re = ""
     _tokens = 0
     _cost = 0
     _log = ""
     with get_openai_callback() as cb:
-        _re = chain.run(_content=_content, _empty=_empty)
+        _re = chain.run(_content=_content)
         _tokens = cb.total_tokens
         _cost = cb.total_cost
         _log += f"\nTokens: {cb.total_tokens} = (Prompt {cb.prompt_tokens} + Completion {cb.completion_tokens})\n"
@@ -37,11 +37,12 @@ Ignore previous instructions. As a marketing strategy analyst, your task is to i
 The customer comments that require marketing strategy analysis are as follows:
 {_content}
 
-The output form of the final result is an array of arrays [{_empty}, {_empty},...], each element in the outermost array is the analysis result corresponding to each comment, and the order of the elements is given order of comments. The analysis result of each comment is the analysis result of 7Ps Marketing Mix in JSON format, with the 7Ps as the main key and the corresponding nouns as the values, ordered as: Product, Promotion, Price, Place, People, Process, Physical evidence. If no information is available for a certain key, do not output the key-value pair.
-Please output the analysis results in English lowercase and only the array:
+For each comment, identify and extract relative nouns of 7Ps from and only from the comment. Output the analysis result of each comment in JSON format in one line, with the 7Ps as the main key and the corresponding nouns as the values. The order of the main key is: Product, Promotion, Price, Place, People, Process, Physical evidence.
+
+Please output the analysis results in English lowercase:
 """
     prompt = PromptTemplate(
-        input_variables=["_content", "_empty"],
+        input_variables=["_content"],
         template=template,
     )
     chain = LLMChain(llm=llm, prompt=prompt)
@@ -69,13 +70,25 @@ Please output the analysis results in English lowercase and only the array:
                 _content = _content + f"{n*N_batch +j +1}) {batch[j]}\n"
             _log += _content
             # print(prompt.format(_content=_content, _empty=r"{}"))
-            [b_re, b_tokens, b_cost, b_log] = call_openai(chain, _content, r"{}")
+            [b_re, b_tokens, b_cost, b_log] = call_openai(chain, _content)
             _log += b_log
             _total_cost += b_cost
             all_re += b_re + "\n"
             # print(b_re)
-    _7P_str = all_re
-    return [_log, _7P_str, _total_cost]
+    _total_cost_str = format(_total_cost, ".5f")
+    _7P_str = all_re.strip()
+    return [_log, _7P_str, _total_cost_str]
+
+
+def parse_7P_str(_str):
+    import re
+    _re = []
+    _li = _str.split("\n")
+    for i in _li:
+        if i:
+            _re.append(re.sub('.+\s*{', '{', i))
+    _re_str = "[" + ",\n".join(_re) + "]"
+    return _re_str
 
 
 def P7_llm(_txt):
@@ -84,9 +97,10 @@ def P7_llm(_txt):
     _7P_str = ""
     _total_cost = 0
     txt_lines = _txt.split("\n")
-    [_log, _7P_str, _total_cost] = P7_openai(key, txt_lines, N_batch)
+    [_log, _7P_str, _total_cost_str] = P7_openai(key, txt_lines, N_batch)
     print(_log)
-    return [_7P_str, str(_total_cost)]
+    _7P_str = parse_7P_str(_7P_str)
+    return [_7P_str, _total_cost_str]
 
 
 
